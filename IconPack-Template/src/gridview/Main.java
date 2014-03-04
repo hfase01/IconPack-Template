a0c3f2dc2b4293c5707105cfec8afea68c074569
Hans Fase <hfase01@gmail.com>
2014-03-03 19:24:13 -0500

    Remove start-up pop-up.

--- IconPack-Template/src/gridview/Main.java
+++ IconPack-Template/src/gridview/Main.java
@@ -23,12 +23,7 @@ package gridview;
 
 import fragments.MainFragment;
 import helper.GlassActionBarHelper;
-import co.w44.h4rsh.AboutDev;
-import co.w44.h4rsh.R;
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -40,6 +35,8 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import co.w44.h4rsh.AboutDev;
+import co.w44.h4rsh.R;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
@@ -115,67 +112,6 @@ public class Main extends SherlockFragmentActivity {
 	 	return CDialog;
 	 }
 
-	/******************************************************************************
-	 ** This code checks if MY OSS is installed on first run. If it is installed **
-	 ** you get a dialog that says you're awesome and the user hits OK to remove **
-	 ** that dialog. If it is NOT installed, the user is prompted to install it. **
-	 ** You can remove this section if you're not checking for apps on first run **
-	 ******************************************************************************/
-	public void onStart() {
-		super.onStart();
-		boolean installed = isAppInstalled("app.the1dynasty.oss");
-		
-		// Checking if installed and if its the first run
-	    if (installed) {
-	    	boolean firstrunOSS = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-	    			.getBoolean("firstrunOSS", true);
-		    if (firstrunOSS){
-		    	
-	    /* 
-	     * Installed dialog
-	     * Check res/values/strings.xml to change text to whatever you want the Alert to say
-	     */
-	        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-	        alert.setTitle(getResources().getString (R.string.alert_start_title));
-	        alert.setMessage(getResources().getString (R.string.alert_start_desc));
-	        alert.setIcon(R.drawable.alert_pass);
-	        alert.setPositiveButton(getResources().getString (R.string.ok), null).show ();
-	        
-		    // Save the state so this dialog doesn't run again
-		    getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-		        .edit()
-		        .putBoolean("firstrunOSS", false) /* You can change this to another name */
-		        .commit();
-	              }
-	    }
-		
-	    /* 
-	     * Not Installed dialog
-	     * Check res/values/strings.xml to change text to whatever you want the Alert to say
-	     */
-		else {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setTitle(getResources().getString (R.string.error_start_title));
-			builder.setMessage(getResources().getString (R.string.error_start_desc));
-	        builder.setIcon(R.drawable.alert_fail);
-			builder.setNeutralButton(getResources().getString (R.string.later), new OnClickListener(){
-				@Override
-				public void onClick(DialogInterface arg0, int arg1) {
-				}
-			});
-			
-		 // Change line 88 with the URL to YOUR app
-			builder.setPositiveButton(getResources().getString (R.string.get), new OnClickListener(){
-				@Override
-				public void onClick(DialogInterface arg0, int arg1) {
-					Intent share = new Intent(Intent.ACTION_VIEW).setData(Uri.parse
-							("market://details?id=co.w44.w44co"));
-		    		startActivity(share);
-			}
-			});
-			builder.show();
-	    }
-	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
